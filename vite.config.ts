d6c06d00100dda8626b757f6b2f43a5d5b29aa99
Tolga Özgün <tolgaozgunn@gmail.com>
2023-11-04 23:17:22 +0300

    Add Dockerfile and setup config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/",
   plugins: [react()],
-})
+  // Required for Docker containers
+  server: {
+    watch: {
+      usePolling: true,
+    },
+    host: true, // needed for the Docker Container port mapping to work
+    strictPort: true,
+    port: 5173, // you can replace this port with any port
+  },
+});
