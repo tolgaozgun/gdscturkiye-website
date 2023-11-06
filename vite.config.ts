de10445664f93456ea99931fa0d9b1cb10414455
Tolga Özgün <tolgaozgunn@gmail.com>
2023-11-07 02:49:40 +0300

    Update base

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "./",
+  base: "./dev",
   plugins: [react()],
   // Required for Docker containers
   server: {
