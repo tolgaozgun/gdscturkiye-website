85e7a27ea82401d634e66fe71dba766978b4a333
Tolga Özgün <tolgaozgunn@gmail.com>
2023-11-10 11:05:52 +0300

    Fix configuration issue

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,23 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: "./dev",
-  plugins: [react()],
-  // Required for Docker containers
-  server: {
-    watch: {
-      usePolling: true,
+export default defineConfig(({ mode }) => {
+  process.env = Object.assign(process.env, loadEnv(mode, process.cwd(), ""));
+
+  console.log(process.env);
+
+  return {
+    base: process.env.VITE_BASE_NAME || "/",
+    plugins: [react()],
+    // Required for Docker containers
+    server: {
+      watch: {
+        usePolling: true,
+      },
+      host: true, // needed for the Docker Container port mapping to work
+      strictPort: true,
+      port: 5173, // you can replace this port with any port
     },
-    host: true, // needed for the Docker Container port mapping to work
-    strictPort: true,
-    port: 5173, // you can replace this port with any port
-  },
+  };
 });
