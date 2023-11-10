e92e10a0ed7d87f697176f60f7a729a81fe313da
Tolga Özgün <tolgaozgunn@gmail.com>
2023-11-10 11:06:27 +0300

    Fix env printing

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig(({ mode }) => {
   process.env = Object.assign(process.env, loadEnv(mode, process.cwd(), ""));
 
-  console.log(process.env);
-
   return {
     base: process.env.VITE_BASE_NAME || "/",
     plugins: [react()],
